0db1352199de49204cf4b5034cbbfe3035ad8160
shahbaz ahmed <shahbazahmed1269@gmail.com>
2017-08-14 11:29:41 +0530

    Created first draft of views xml

---
+++ app/src/main/java/com/example/shahbazahmed/loginmvvmdatabinding/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.shahbazahmed.loginmvvmdatabinding;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button btnRegister = findViewById(R.id.btn_create);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+        TextView tvPasswordForgot = findViewById(R.id.tv_password_forgot);
+        tvPasswordForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
